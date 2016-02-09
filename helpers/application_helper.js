82d84710a6a43184ce99a5b3076772287d408b9a
David Eibich <david.eibich@software-berater.net>
2016-02-09 17:28:28 +0100

    Mac Adresse auch unter Linux

--- helpers/application_helper.js
+++ helpers/application_helper.js
@@ -1,34 +1,46 @@
 var exec = require("child_process").exec;
-
+var os = require("os");
 var ApplicationHelper = function() { };
 
-ApplicationHelper.prototype.ifconfig = function(callback){
-  exec("ifconfig", function (err, stdout, stderr) {
-    if (err) {
-      callback(err, stderr);
-    } else {
-      // ifconfig emits a kind of mish-mash formats, the following gobbledygook turns that into nested objects in
-      // a (hopefully) intuitive way (see sample below). 
-      var results = stdout.split('\n').reduce(function (a, line) {
-          if (/^\t/.test(line)) { // tabs indicate "features" of previous unindented line
-            a[a.length - 1].push(line.replace(/^\t/,'').trim());
-          } else if (line) {
-            a.push(line.split(':').map(function (s) { return s.trim(); })); // unindented lines start with a name followed by a :
-          }
-          return a;
-        }, []).reduce(function (o, a) {
-          o[a.shift()] = a.map(function (l) {
-            return l.match(/^([^ =:]+)[ =:](?:[ ]?)(.*)$/).slice(1,3); // indented lines start with a name followed by a space, :, or =
-          }).reduce(function (p,b) {
-            p[b[0]] = b[1];
-            return p;
-          }, {});
-          return o;
-          }, {});
-      callback(null, results);
-    }
-  });
-
+ApplicationHelper.prototype.getMac = function(callback){
+  switch(os.platform()){
+    case 'darwin':
+      exec("ifconfig", function (err, stdout, stderr) {
+        if (err) {
+          callback(err, stderr);
+        } else {
+          var results = stdout.split('\n').reduce(function (a, line) {
+              if (/^\t/.test(line)) {
+                a[a.length - 1].push(line.replace(/^\t/,'').trim());
+              } else if (line) {
+                a.push(line.split(':').map(function (s) { return s.trim(); })); 
+              }
+              return a;
+            }, []).reduce(function (o, a) {
+              o[a.shift()] = a.map(function (l) {
+                return l.match(/^([^ =:]+)[ =:](?:[ ]?)(.*)$/).slice(1,3); 
+              }).reduce(function (p,b) {
+                p[b[0]] = b[1];
+                return p;
+              }, {});
+              return o;
+              }, {});
+          callback(null, results['en0']['ether'].split(':').join(''));
+        }
+      });
+      break;
+    default:
+      exec('ip link show eth0', function(err, stdout, stderr){
+        if(err){
+          callback(err, stderr);
+        } else {
+          matches = stdout.match(/(([A-Fa-f0-9]{2}[:]){5}[A-Fa-f0-9]{2}[,]?)/);
+          callback(null, matches[0].split(':').join(''));
+        }
+      });
+  }
 };
+
+
 var apphelper = new ApplicationHelper();
 module.exports = apphelper;
