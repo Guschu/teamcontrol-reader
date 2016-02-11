5fe40da9776ec047dd9f3460b4aca942654f07a2
David Eibich <david.eibich@software-berater.net>
2016-02-11 14:27:47 +0100

    Html und CSS

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,6 @@
 var socket = io.connect( "http://localhost:8080" );
 
 socket.on("terminalContent", function(content) {
-  document.getElementsByClassName('feedback')[0].className = content['feedback'];
-  document.getElementsByClassName('content')[0].innerHTML = content['text'];
+  document.getElementById('status').className = content['status'];
+  document.getElementById('message').innerHTML = content['message'];
 });
