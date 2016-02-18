798a3cebfba49189d3ca67f50d68f8ee77325ef9
David Eibich <david.eibich@software-berater.net>
2016-02-18 11:57:01 +0100

    Styles

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,5 +2,5 @@ var socket = io.connect( "http://localhost:8080" );
 
 socket.on("terminalContent", function(content) {
   document.getElementById('status').className = content['status'];
-  document.getElementById('message').innerHTML = content['message'];
+  document.getElementById('status').innerHTML = content['message'];
 });
