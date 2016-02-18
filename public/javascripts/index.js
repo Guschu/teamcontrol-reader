7952c252251c4683a6b5f051f6c9b13a7c04a68c
David Eibich <david.eibich@software-berater.net>
2016-02-18 12:31:04 +0100

    Fehleranzeige, wenn Node nicht mehr läuft.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,3 +4,8 @@ socket.on("terminalContent", function(content) {
   document.getElementById('status').className = content['status'];
   document.getElementById('status').innerHTML = content['message'];
 });
+
+socket.on('disconnect', function() {
+  document.getElementById('status').className = 'error';
+  document.getElementById('status').innerHTML = 'Node-Server inaktiv.';  
+});
